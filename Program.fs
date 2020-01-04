54f5353c29bf379dcb90751f7ff8056637c5ee8d
Manoj Waikar <manoj.waikar@intercax.com>
2020-01-04 19:40:58 +0530

    First incomplete attempt at reading excel data.

--- Program.fs
+++ Program.fs
@@ -1,8 +1,11 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
+open ExcelUtil
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
+    let q1 = readSheet (Sheet2015())
+    printfn "%A" q1
     0 // return an integer exit code
